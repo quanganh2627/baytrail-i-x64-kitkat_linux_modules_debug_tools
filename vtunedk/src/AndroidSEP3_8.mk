fc053c341968b53b85a6bcf2eb24795c23427cd9
Christophe Guerard <christophe.guerard@intel.com>
2012-12-21 15:58:28 +0100

    vtunedk: Re-enable sep 3.8 on MR1
    BZ: 76457
    
    Change-Id: I3f456d8c479f17a54502939bdcc35ad8d4faadd2
    Signed-off-by: Christophe Guerard <christophe.guerard@intel.com>
    Reviewed-on: http://android.intel.com:8080/83479
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- vtunedk/src/AndroidSEP3_8.mk
+++ vtunedk/src/AndroidSEP3_8.mk
@@ -2,4 +2,8 @@
 vtunedk=1
 .PHONY: sep3_8
 sep3_8: build_kernel
-	TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" vendor/intel/support/debugtools-build.sh -c $(TARGET_DEVICE) -M device/intel/debug_tools/vtunedk/src
+	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" \
+	TARGET_DEVICE="$(TARGET_DEVICE)" \
+	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
+	vendor/intel/support/kernel-build.sh \
+	-M device/intel/debug_tools/vtunedk/src -f "BOARD_HAVE_SMALL_RAM=$(BOARD_HAVE_SMALL_RAM)"
--- vtunedk/src/vtsspp/AndroidVTSSPP.mk
+++ vtunedk/src/vtsspp/AndroidVTSSPP.mk
@@ -1,5 +1,8 @@
 # This makefile is included from vendor/intel/*/AndroidBoard.mk.
-
 .PHONY: vtsspp
 vtsspp: build_kernel
-	TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" vendor/intel/support/debugtools-build.sh -c $(TARGET_DEVICE) -M device/intel/debug_tools/vtunedk/src/vtsspp
+	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" \
+	TARGET_DEVICE="$(TARGET_DEVICE)" \
+	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
+	vendor/intel/support/kernel-build.sh \
+	-M device/intel/debug_tools/vtunedk/src/vtsspp
