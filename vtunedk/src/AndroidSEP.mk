90dfcefa02d46fda130c70d767e00665fffda44d
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-05-16 18:14:39 +0200

    Remove Deprecated checks and clean Makefiles
    BZ: 108694
    
    Remove check to TARGET_KERNEL_SOURCE_IS_PRESENT
    (it's now handled in kernel/AndroidKernel.mk automatically)
    Remove old variable that are not used anymore
    
    Change-Id: I14e2989e7c83c50e5ccacb49495c10c76bef04b0
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: http://android.intel.com:8080/107993
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- vtunedk/src/AndroidSEP.mk
+++ vtunedk/src/AndroidSEP.mk
@@ -1,5 +1,3 @@
 # This makefile is included from vendor/intel/*/AndroidBoard.mk.
-vtunedk=true
-sep_version=sep3_10
 $(eval $(call build_kernel_module,$(call my-dir),sep3_10,"BOARD_HAVE_SMALL_RAM=$(BOARD_HAVE_SMALL_RAM)"))
 
--- vtunedk/src/pax/AndroidPAX.mk
+++ vtunedk/src/pax/AndroidPAX.mk
@@ -1,4 +1,3 @@
 # This makefile is included from vendor/intel/*/AndroidBoard.mk.
-pax=1
 $(eval $(call build_kernel_module,$(call my-dir),pax))
 
