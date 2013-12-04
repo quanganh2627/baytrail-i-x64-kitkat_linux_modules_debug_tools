1d869c621cb69f007fb4020d3562a66e0ea774ab
Kirill Uhanov <kirill.uhanov@intel.com>
2013-12-04 04:47:00 -0500

    synchronization otc and mcg vtunedk
    BZ:156536
    
    Currently different version of vtunedk are lokated in mcg and otc trees.
    (otc includes fresh sep driver from vtunedk).
    To have ability to use one vtune I need to synchronize trees.
    
    Change-Id: Ib0a37474ba04dfec2ad7f31b49563131aeecd96c
    Signed-off-by: Kirill Uhanov <kirill.uhanov@intel.com>

--- vtunedk/src/vtsspp/module.c
+++ vtunedk/src/vtsspp/module.c
@@ -72,18 +72,28 @@ int vtss_check_trace(const char* func_name, int* flag)
 
 #if defined(CONFIG_X86_32)
 #define VTSS_SYMBOL_SCHED_SWITCH  "__switch_to"
+#define VTSS_SYMBOL_SCHED_SWITCH_AUX  "context_switch"
 #elif defined(CONFIG_X86_64)
 #define VTSS_SYMBOL_SCHED_SWITCH  "context_switch"
+#define VTSS_SYMBOL_SCHED_SWITCH_AUX  "__switch_to"
 #endif
+
 #define VTSS_SYMBOL_PROC_FORK     "do_fork"
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,9,0)
 #define VTSS_SYMBOL_PROC_EXEC     "do_execve"
 #else
-// from the version 3.9 do_execve is inlined into sys_execve, and brobe is broken because of this.
+// from the version 3.9 do_execve is inlined into sys_execve, and probe is broken because of this.
 // lp tried to use sys_execve instead, but this crashes ia32 bit systems
-// so, we are using do_execve_common now.
+// so, we are using do_execve_common for 32 bit.
+// The only possible solution dor intel64 is use sys_execve
+#if defined(CONFIG_X86_32)
 #define VTSS_SYMBOL_PROC_EXEC     "do_execve"
+#else
+#define VTSS_SYMBOL_PROC_EXEC     "sys_execve"
 #endif
+#endif
+
 #define VTSS_SYMBOL_PROC_EXIT     "do_exit"
 #define VTSS_SYMBOL_MMAP_REGION   "mmap_region"
 #ifdef VTSS_SYSCALL_TRACE
@@ -111,6 +121,7 @@ static void tp_sched_switch(VTSS_TP_PROTO VTSS_TP_RQ struct task_struct *prev, s
 {
    void* prev_bp = NULL;
    void* prev_ip = NULL;
+//   printk("switch\n");
    if (prev == current && current !=0 )
    {
        unsigned long bp;
@@ -397,9 +408,14 @@ static int unprobe_##name(void) \
 
 /* ------------------------------------------------------------------------- */
 /* Define jprobe stub */
-#define DEFINE_JP_STUB(name,symbol) \
+#define DEFINE_JP_STUB(name,symbol,symbol_aux) \
 static struct jprobe _jp_##name = { \
     _SET_KP_SYMBOL_NAME(symbol) \
+    .kp.addr = (kprobe_opcode_t*)NULL, \
+    .entry = (kprobe_opcode_t*)jp_##name \
+}; \
+static struct jprobe _jp_##name_aux = { \
+    _SET_KP_SYMBOL_NAME(symbol_aux) \
     .kp.addr = (kprobe_opcode_t*)NULL, \
     .entry = (kprobe_opcode_t*)jp_##name \
 }; \
@@ -412,8 +428,15 @@ static int probe_##name(void) \
         { \
             _SET_KPROBE_FLAGS(_jp_##name.kp) \
             rc = register_jprobe(&_jp_##name); \
-            if (rc) ERROR("register_jprobe('%s') failed: %d", symbol, rc); \
         } \
+        if (rc){\
+        _LOOKUP_SYMBOL_NAME(_jp_##name_aux.kp,symbol_aux) \
+        { \
+            _SET_KPROBE_FLAGS(_jp_##name_aux.kp) \
+            rc = register_jprobe(&_jp_##name_aux); \
+        }\
+        } \
+        if (rc) ERROR("register_jprobe('%s') failed: %d", symbol, rc); \
     } \
     return rc; \
 } \
@@ -486,7 +509,7 @@ DEFINE_KP_STUB(syscall_leave,      VTSS_SYMBOL_SYSCALL_LEAVE)
     rc = unregister_trace_##name(tp_##name VTSS_TP_DATA);
 #endif
 
-DEFINE_JP_STUB(sched_switch,       VTSS_SYMBOL_SCHED_SWITCH)
+DEFINE_JP_STUB(sched_switch, VTSS_SYMBOL_SCHED_SWITCH, VTSS_SYMBOL_SCHED_SWITCH_AUX)
 DEFINE_RP_STUB(sched_process_fork, VTSS_SYMBOL_PROC_FORK, 0)
 DEFINE_KP_STUB(sched_process_exit, VTSS_SYMBOL_PROC_EXIT)
 
