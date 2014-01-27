92c639fa8099f4933dd075e035a59becd0f6ef7b
Michael Bergeron <michael.bergeron@intel.com>
2014-01-27 10:21:35 -0800

    Cleanup socwatch module makefile
    More clearly defined that CLEAR_VARS is needed at the module level, and
    not the file level. Kernel modules will now default to "optional", which
    is the correct setting for these external facing modules; inclusion is
    controlled by a new debug-tools mixin.
    
    For: APDEV-1194
    Change-Id: I557f51471b86500dce6ddbc7651d994632e7065d
    Signed-off-by: Michael Bergeron <michael.bergeron@intel.com>

--- socwatchdk/Android.mk
+++ socwatchdk/Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := socwatch1_3_external
 LOCAL_MODULE_PATH := $(LOCAL_PATH)/src
-LOCAL_MODULE_TAGS := debug
 include $(BUILD_EXTERNAL_KERNEL_MODULE)
