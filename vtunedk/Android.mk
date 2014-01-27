ec464d4971ca29d52b9983416fd6f9e8c747f516
Hans Odeberg <hans.odeberg@intel.com>
2014-01-27 11:08:19 +0100

    DEBUG_TOOLS: Align with ABT build system for external modules
    The ABT method for building external kernel modules is about to be modified.
    The modification will require each module to start with
     'include $(CLEAR_VARS)' (which is the norm for other Android modules).
    
    The build system will also start to copy each module's source tree
    to a unique location before build. Thus the pax module, which tries
    to include header files outside its source tree using relative paths,
    will fail to build. This is fixed by using the LOCAL_C_INCLUDES variable
    to tell the build system where to look for header files.
    
    Change-Id: Ie758d18031dac2b09eaad7b7579a94c4c1e45ffc
    For: APDEV-1257
    Signed-off-by: Hans Odeberg <hans.odeberg@intel.com>

--- vtunedk/Android.mk
+++ vtunedk/Android.mk
@@ -1,14 +1,20 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := sep3_10
 LOCAL_MODULE_PATH := $(LOCAL_PATH)/sepdk
 include $(BUILD_EXTERNAL_KERNEL_MODULE)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := pax
+# As the sepdk/pax directory is copied to a new location, some
+# extra include directives are needed to tell the compiler
+# where to find the include directories.
+LOCAL_C_INCLUDES := $(addprefix $(LOCAL_PATH)/sepdk/,include inc)
 LOCAL_MODULE_PATH := $(LOCAL_PATH)/sepdk/pax
 include $(BUILD_EXTERNAL_KERNEL_MODULE)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := vtsspp
 LOCAL_MODULE_PATH := $(LOCAL_PATH)/sepdk/src/vtsspp
 include $(BUILD_EXTERNAL_KERNEL_MODULE)
