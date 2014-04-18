ca68f8e58154c0f80fc7b8bb95eb16e81868e277
Kirill Uhanov <kirill.uhanov@intel.com>
2014-04-18 04:55:42 -0400

    move vtunedk to sep3_15 for new platforms' supporting
    BZ: 187399
    
    move vtunedk to sep3_15 for new platforms'
    supporting: Moorfield
    
    Change-Id: I6105609925a3bb4ab3278b19b30f50c599dc1123
    Signed-off-by: Kirill Uhanov <kirill.uhanov@intel.com>

--- vtunedk/src/inc/skl_unc_cbo.h
+++ vtunedk/src/inc/skl_unc_cbo.h
@@ -1,21 +1,21 @@
 /*
-    Copyright (C) 2005-2012 Intel Corporation.  All Rights Reserved.
- 
+    Copyright (C) 2011-2014 Intel Corporation.  All Rights Reserved.
+
     This file is part of SEP Development Kit
- 
+
     SEP Development Kit is free software; you can redistribute it
     and/or modify it under the terms of the GNU General Public License
     version 2 as published by the Free Software Foundation.
- 
+
     SEP Development Kit is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with SEP Development Kit; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- 
+
     As a special exception, you may use this file as part of a free software
     library without restriction.  Specifically, if other files instantiate
     templates or use macros or inline functions from this file, or you compile
@@ -26,12 +26,17 @@
     the GNU General Public License.
 */
 
-#ifndef _CORE_H_
-#define _CORE_H_
+#ifndef _SKLUNC_CBO_H_INC_
+#define _SKLUNC_CBO_H_INC_
 
-#include "msrdefs.h"
-#define EM_INT_MASK             0x00100000
+/*
+ * Local to this architecture: SKL uncore CBO unit 
+ * Arch Perf monitoring version 4
+ */
+// CBO MSRs
+#define SKL_CBO_PERF_GLOBAL_CTRL        0xE01
+#define SKL_CBO_PERF_GLOBAL_STATUS      0xE02
 
-extern DISPATCH_NODE  core_dispatch;
+extern DISPATCH_NODE  skylakeunc_cbo_dispatch;
 
 #endif
